c6002d00fce50654ee65b5abd55857b0d4e512f1
Varut Rujitanont <arzachin@gmail.com>
2025-04-09 14:44:56 +0700

    add  ddqn and dualing

--- agent.py
+++ agent.py
@@ -13,6 +13,7 @@ import random
 import torch.nn as nn
 import os
 import matplotlib
+import psutil
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 RUNS_DIR = "runs"
@@ -21,12 +22,13 @@ os.makedirs(RUNS_DIR, exist_ok=True)
 matplotlib.use('Agg')
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+torch.set_num_threads(18)
 class Agent:
     def __init__(self, hyperparameters_set):
         with open('hyperparameters.yml', 'r') as file:
             all_hyperparameters_sets = yaml.safe_load(file)
             hyperparameters = all_hyperparameters_sets[hyperparameters_set]
-
+        self.env_id             = hyperparameters['env_id']
         self.replay_memory_size = hyperparameters['replay_memory_size']
         self.mini_batch_size = hyperparameters['mini_batch_size']
         self.epsilon_init = hyperparameters['epsilon_init']
@@ -38,6 +40,9 @@ class Agent:
         self.stop_on_reward = hyperparameters['stop_on_reward']
         self.fc1_nodes = hyperparameters['fc1_nodes']
         self.env_make_params = hyperparameters.get('env_make_params',{})
+        self.enable_double_dqn = hyperparameters['enable_double_dqn']
+        self.enable_dueling_dqn = hyperparameters['enable_dueling_dqn']
+        self.pretrained_model = hyperparameters.get('pretrained_model', None)
 
         self.loss_fn = nn.MSELoss()
         self.optimizer = None
@@ -50,29 +55,39 @@ class Agent:
     def run(self,is_training=True,render=False):
 
         if is_training:
+            log_message = f"DQN\n"
+            if self.pretrained_model and os.path.exists(self.pretrained_model):
+                policy_dqn.load_state_dict(torch.load(self.pretrained_model))
+                target_dqn.load_state_dict(policy_dqn.state_dict())
+                print(f"Loaded pretrained model from {self.pretrained_model}")
+            if self.enable_double_dqn == True:
+                log_message = f"Double DQN Enable"
+            print(log_message)
+            with open(self.LOG_FILE, 'w') as log_file:
+                log_file.write(log_message+ '\n')
             start_time = datetime.datetime.now()
             last_graph_update_time = start_time
-            log_message = f"Start time: {start_time}\n"
+            log_message = f"Start time: {start_time}"
             print(log_message)
             with open(self.LOG_FILE, 'w') as log_file:
                 log_file.write(log_message+ '\n')
 
         # env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None, use_lidar=False)
-        env = gymnasium.make("CartPole-v1", render_mode="human" if render else None)
+        env = gymnasium.make(self.env_id, render_mode='human' if render else None, **self.env_make_params)
         
 
         num_states = env.observation_space.shape[0]
         num_actions = env.action_space.n
         reward_per_episode = []
         epsilon_history = []
-
-        policy_dqn = DQN(num_states,num_actions,self.fc1_nodes ).to(device)
+        optimize_every_n_steps = 10
+        policy_dqn = DQN(num_states,num_actions,self.fc1_nodes,self.enable_dueling_dqn ).to(device)
 
         if is_training:
             memory = ReplayMemory(self.replay_memory_size)
             epsilon = self.epsilon_init
             
-            target_dqn = DQN(num_states,num_actions,self.fc1_nodes  ).to(device)
+            target_dqn = DQN(num_states,num_actions,self.fc1_nodes,self.enable_dueling_dqn  ).to(device)
             target_dqn.load_state_dict(policy_dqn.state_dict())
 
             step_count = 0
@@ -88,7 +103,7 @@ class Agent:
         #  Training loop
         for episode in itertools.count():
             state, _ = env.reset()
-            state = torch.tensor(state,dtype=torch.float,device=device)
+            state = torch.as_tensor(state,dtype=torch.float,device=device)
 
             terminated = False
             episode_reward = 0.0
@@ -98,7 +113,7 @@ class Agent:
                 # (feed the observation to your agent here)
                 if is_training and random.random() < epsilon:
                     action = env.action_space.sample()
-                    action = torch.tensor(action,dtype=torch.long,device=device)
+                    action = torch.as_tensor(action,dtype=torch.long,device=device)
                 else:
                     with torch.no_grad():
                         action = policy_dqn(state.unsqueeze(dim=0)).squeeze().argmax()
@@ -108,14 +123,24 @@ class Agent:
                 
                 episode_reward += reward
 
-                new_state = torch.tensor(new_state,dtype=torch.float,device=device)
-                reward = torch.tensor(reward,dtype=torch.float,device=device)
+                new_state = torch.as_tensor(new_state,dtype=torch.float,device=device)
+                reward = torch.as_tensor(reward,dtype=torch.float,device=device)
                 
 
                 if is_training:
-                    memory.append((state,action,new_state,reward,terminated))
+                    memory.append((state.detach(),action.detach() if action.requires_grad else action,new_state.detach(),reward.detach(),terminated))
                     step_count += 1
+                    if step_count % optimize_every_n_steps == 0:
+                        if len(memory) > self.mini_batch_size:
 
+                            #sample from memory
+                            mini_batch = memory.sample(self.mini_batch_size)
+                            self.optimize(mini_batch,policy_dqn,target_dqn)
+
+                            
+                            if step_count > self.network_sync_rate:
+                                target_dqn.load_state_dict(policy_dqn.state_dict())
+                                step_count = 0
                 state = new_state
 
             reward_per_episode.append(episode_reward)
@@ -123,27 +148,38 @@ class Agent:
             if is_training:
                 if episode_reward > best_reward:
                     log_message = f"{datetime.datetime.now().strftime(DATE_FORMAT)}: New best reward: {episode_reward} at episode {episode}"
+                    print(f"Memory used: {psutil.virtual_memory().used / (1024 ** 3):.2f} GB")
                     print(log_message)
                     with open(self.LOG_FILE, 'a') as log_file:
                         log_file.write(log_message + '\n')
+
+                    torch.save(policy_dqn.state_dict(), self.MODEL_FILE)
                     best_reward = episode_reward
 
                 current_time = datetime.datetime.now()
-                if current_time - last_graph_update_time > datetime.timedelta(seconds=10):
+                if current_time - last_graph_update_time > datetime.timedelta(seconds=60):
                     self.save_graph(reward_per_episode, epsilon_history)
                     last_graph_update_time = current_time
-
-
                 if len(memory) > self.mini_batch_size:
-
-                    #sample from memory
+                                            #sample from memory
                     mini_batch = memory.sample(self.mini_batch_size)
                     self.optimize(mini_batch,policy_dqn,target_dqn)
                     epsilon = max(epsilon * self.epsilon_decay,self.epsilon_min)
                     epsilon_history.append(epsilon)
-                    if step_count > self.network_sync_rate == 0:
+                    
+                    if step_count > self.network_sync_rate:
                         target_dqn.load_state_dict(policy_dqn.state_dict())
                         step_count = 0
+                    
+            env.close() 
+            # if epsilon <= self.epsilon_min:
+            #     log_message = f"{datetime.datetime.now().strftime(DATE_FORMAT)}: Epsilon decay finished at episode {episode}"
+            #     print(log_message)
+            #     with open(self.LOG_FILE, 'a') as log_file:
+            #         log_file.write(log_message + '\n')
+            #     break
+
+
     def save_graph(self,reward_per_episode, epsilon_history):
         fig = plt.figure(1)
 
@@ -162,7 +198,7 @@ class Agent:
         plt.subplots_adjust(wspace=1.0, hspace=1.0)
 
         fig.savefig(self.GRAPH_FILE)
-        plt.close(fig)
+        plt.close()
 
 
     def optimize(self, mini_batch, policy_dqn, target_dqn):
@@ -172,9 +208,14 @@ class Agent:
         actions = torch.stack(actions).to(device)   
         new_states = torch.stack(new_states).to(device) 
         rewards = torch.stack(rewards).to(device)   
-        terminations = torch.tensor(terminations).float().to(device)
+        terminations = torch.as_tensor(terminations).float().to(device)
         with torch.no_grad():
-            target_q = rewards + (1-terminations )* self.discount_factor_g  * target_dqn(new_states).max(dim=1)[0]
+
+            if self.enable_double_dqn:
+                best_actions_from_policy = policy_dqn(new_states).argmax(dim=1)
+                target_q = rewards + (1-terminations )* self.discount_factor_g  * target_dqn(new_states).gather(dim=1,index=best_actions_from_policy.unsqueeze(dim=1)).squeeze()
+            else:
+                target_q = rewards + (1-terminations )* self.discount_factor_g  * target_dqn(new_states).max(dim=1)[0]
 
 
         current_q = policy_dqn(states).gather(dim=1,index=actions.unsqueeze(dim=1)).squeeze()
